8bc28070fea424a0df6c511168c25572b7c2e1e8
Aniket Jagtap <aniket.jagtap@connectedlife.io>
2025-03-11 18:32:55 +0530

    comments

--- api/patient/route.js
+++ api/patient/route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const patientController = require('./controller');
 
-// CRUD routes
 router.post('/', patientController.createPatient);
 router.get('/', patientController.getPatients);
 router.get('/:id', patientController.getPatientById);
